6ba2ba11d5669c31c2da8c7e8164c565e52dcb31
Parralol <110953563+Parralol@users.noreply.github.com>
2022-09-30 12:56:20 -0500

    hasta ciclo 1
    vida hpta

---
+++ replicate Second wind/domain/AManufacturing.java
@@ -0,0 +1,67 @@
+package domain;
+import java.util.*;
+
+
+/*No olviden adicionar la documentacion*/
+public class AManufacturing{
+    static private int SIZE=50;
+    private Thing[][] lattice;
+    
+    public AManufacturing() {
+        lattice=new Thing[SIZE][SIZE];
+        for (int r=0;r<SIZE;r++){
+            for (int c=0;c<SIZE;c++){
+                lattice[r][c]=null;
+            }
+        }
+        someThings();
+    }
+
+    public int  getSize(){
+        return SIZE;
+    }
+
+    public Thing getThing(int r,int c){
+        return lattice[r][c];
+    }
+
+    public void setThing(int r, int c, Thing e){
+        lattice[r][c]=e;
+    }
+
+    public void someThings(){
+    Cell simba = new Cell(this, 1,1, true);
+    Cell dala = new Cell(this, 2,2, true);
+    }
+    
+    public int neighborsActive(int r, int c){
+        int num=0;
+        for(int dr=-1; dr<2;dr++){
+            for (int dc=-1; dc<2;dc++){
+                if ((dr!=0 || dc!=0) && inLatice(r+dr,c+dc) && 
+                    (lattice[r+dr][c+dc]!=null) &&  (lattice[r+dr][c+dc].isActive())) num++;
+            }
+        }
+        return (inLatice(r,c) ? num : 0);
+    }
+   
+
+    
+    public boolean isEmpty(int r, int c){
+        return (inLatice(r,c) && lattice[r][c]==null);
+    }    
+        
+    private boolean inLatice(int r, int c){
+        return ((0<=r) && (r<SIZE) && (0<=c) && (c<SIZE));
+    }
+    
+   
+    public void ticTac(){
+        for(Thing[] a: lattice){
+            for(Thing b: a){
+                b.change();
+            }
+        }
+    }
+
+}
